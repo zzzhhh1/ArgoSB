d4e7c473ff3af495c411ac53ff8d96ab14ecfe6d
甬哥侃侃侃ygkkk <121604513+yonggekkk@users.noreply.github.com>
2025-04-28 14:58:01 +0800

    Update argosb.sh

--- argosb.sh
+++ argosb.sh
@@ -100,7 +100,7 @@ echo "CPU架构：$cpu"
 echo "ArgoSB脚本未安装，开始安装…………" && sleep 3
 echo
 else
-echo "ArgoSB脚本未启动，请先将脚本卸载(agsb del)，再重新安装ArgoSB脚本"
+echo "ArgoSB脚本未启动，可能与其他脚本冲突了，请先将脚本卸载(agsb del)，再重新安装ArgoSB脚本"
 exit
 fi
 
